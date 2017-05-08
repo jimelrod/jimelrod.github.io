a49768814c18be31e67e2a413d464282a2c65016
Jim Elrod <elrod.development@gmail.com>
2017-05-07 19:49:13 -0500

    Code cleanup

--- audio.js
+++ audio.js
@@ -1,18 +1,3 @@
-// DEBUGGING ONLY!!!!!
-
-
-
-// for (let i = 0; i < 256; i++) {
-//     let cell = document.createElement("div");
-//     cell.id = `cell${i}`;
-//     cell.classList.add("cell");
-
-//     document
-//         .querySelectorAll('.visual-tester')[0]
-//         .appendChild(cell);
-// }
-
-
 navigator.getUserMedia({
     audio: true
 }, stream => EODG.handleStream(stream), err => console.log(err));
@@ -32,73 +17,41 @@ EODG.handleStream = stream => {
     analyser.fftSize = 1024;
 
     let frequencyArray = new Uint8Array(analyser.frequencyBinCount);
-
-    //analyser.getByteFrequencyData(frequencyArray);
-
-    //console.log(frequencyArray);
-
-    // while (true) {
-    // //for (var x = 0; x < 1000; x++) {
-    //     analyser.getByteFrequencyData(frequencyArray);
-    //     updateCells(frequencyArray);
-    //     // for (let i = 0; i < 256; i++) {
-            
-    //     //     // let ele = document.getElementById(`cell${i}`);
-    //     //     // ele.style.backgroundColor = `rgb(${frequencyArray[i]}, 0, 0)`;
-    //     //     // ele.innerHTML = frequencyArray[i];
-
-    //     //     //console.log(`${i} - ${frequencyArray[i]}`);
-
-
-    //     // }
-
-    // }
-
-    let timeData = new Uint8Array(1024);
-    // analyser.getByteTimeDomainData(timeData);
-    // console.log(timeData);
+    let timeData = new Uint8Array(analyser.fftSize);
 
     function doDraw() {
          requestAnimationFrame(doDraw);
-        // analyser.getByteFrequencyData(frequencyArray);
-        // //updateCells(frequencyArray);
-        // let sum = 0;
-        // for (let i = 0; i < 256; i++) {
-        //     sum += frequencyArray[i];
-        // }
-        // let average = sum / 256;
 
-
-        // document
-        //     .querySelectorAll('.visual-tester')[0]
-        //     .style
-        //     .backgroundColor = `rgb(${average}, ${average}, ${average})`;
-
-
-        // Stolen from https://gist.github.com/kevincennis/6149078
+        // Pretty much stolen from https://gist.github.com/kevincennis/6149078
 
         var total = i = 0
             , percentage
             , float
             , rms
             , db;
+
         analyser.getByteTimeDomainData(timeData);
+
+        // We may use freq data instead... not sure...
+        // analyser.getByteFrequencyData(frequencyArray);
+
         while ( i < 1024 ) {
             float = ( timeData[i++] / 0x80 ) - 1;
             total += ( float * float );
         }
+
         rms = Math.sqrt(total / 1024);
         db  = 20 * ( Math.log(rms) / Math.log(10) );
+
         // sanity check
         db = Math.max(-48, Math.min(db, 0));
         percentage = 100 + ( db * 2.083 );
-        let average = Math.floor(percentage);
         
         document
             .getElementById('mask')
             .style
-            // .opacity = 1 - average/500;
-            .opacity = average/200;
+            .opacity = Math.floor(percentage)/200;
     }
+    
     doDraw();
 };
